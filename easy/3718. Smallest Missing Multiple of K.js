676eb6eee4c03b7335f1cc356008ff980235077c
Mikalai Yakimau <kolavadimovih@gmail.com>
2026-08-25 12:53:03 +0200

    Add many projects easy and medium

---
+++ easy/3718. Smallest Missing Multiple of K.js
@@ -0,0 +1,45 @@
+/*Given an integer array nums and an integer k, return the smallest positive multiple of k that is missing from nums.
+
+A multiple of k is any positive integer divisible by k.
+
+
+
+Example 1:
+
+Input: nums = [8,2,3,4,6], k = 2
+
+Output: 10
+
+Explanation:
+
+The multiples of k = 2 are 2, 4, 6, 8, 10, 12... and the smallest multiple missing from nums is 10.
+
+Example 2:
+
+Input: nums = [1,4,7,10,15], k = 5
+
+Output: 5
+
+Explanation:
+
+The multiples of k = 5 are 5, 10, 15, 20... and the smallest multiple missing from nums is 5.
+
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+1 <= k <= 100*/
+
+var missingMultiple = function(nums, k) {
+    let i = 1
+    while (true) {
+        let num = k*i
+        if(nums.includes(num)){
+            i++
+            continue
+        }else return num
+    }
+};
+console.log(missingMultiple([1,4,7,10,15], 5))
